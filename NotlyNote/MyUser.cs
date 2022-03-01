832a4ca0f347aa3f7b0c3b3432bf0461c1ad5e39
RicardoFeCarva <ricardinho_carvalheira@hotmail.com>
2022-03-01 15:30:43 -0500

    UserCreated

---
+++ NotlyNote/MyUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotlyNote
+{
+    public class MyUser
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserPassword { get; set; }    
+        public int UserId { get; set; }
+        public DateTime UserCreated { get; set; }
+        public string UserPhoneNumber { get; set; }
+
+        private string _UserEmail;
+        public string UserEmail 
+        {
+            get { return _UserEmail; }
+            set
+            {
+                if(value.Contains("@"))
+                {
+                    _UserEmail = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Email Address");
+                }
+            }
+                
+        }
+
+
+
+        public MyUser()
+        {
+            UserFirstName = "Untitled";
+            UserLastName = "User";
+            UserPassword = "password";
+        }
+
+
+    }
+}
