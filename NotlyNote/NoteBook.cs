b13f451e4ae4f2f40d3258dc69b93010780facd1
ricardocarva <ricardofecarva@gmail.com>
2022-03-04 06:47:30 -0500

    Created NoteBook

---
+++ NotlyNote/NoteBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotlyNote
+{
+    public class NoteBook
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
